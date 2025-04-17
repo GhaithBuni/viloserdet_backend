43d0f5f278c5bb081a2797b488da7a3bc6dca80c
Ghaith <ghaith.buni122@gmail.com>
2025-04-18 00:25:15 +0200

    add Core

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 4000;
 
 const allowedOrigins = [
-  "http://localhost:3000",
+  "hhttps://viloserdet.vercel.app",
   "http://localhost:3001", // Add your production domain here
 ];
 
