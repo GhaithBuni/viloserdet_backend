11bdd86b5f0e39eb1d16e4b0ecfc76e433802775
Ghaith <ghaith.buni122@gmail.com>
2025-07-17 20:59:06 +0200

    Change Date to String

--- models/cleanbooking.js
+++ models/cleanbooking.js
@@ -6,7 +6,7 @@ const bookingSchema = new mongoose.Schema({
   personalNumber: { type: String, required: true }, // Changed from 'personnumer' & set to String (Swedish personal numbers contain dashes)
   email: { type: String, required: true },
   phone: { type: String, required: true }, // Changed from Number to String (phone numbers can start with '0' and contain '+')
-  movingDay: { type: Date, required: true },
+  movingDay: { type: String, required: true },
   adress: { type: String, required: true }, // originAdress renamed for clarity
   message: { type: String }, // Optional field from formData
   houseSpace: { type: String }, // Optional field for house space from formData
